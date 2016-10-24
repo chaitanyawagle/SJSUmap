718171e2082a4dda739b0f45249f52b3e14e816d
SDCORP\33843 <chaitanya.wagle@gmail.com>
2016-10-23 18:08:52 -0700

    Merge branch 'master' of https://github.com/chaitanyawagle/SJSUmap
    # Conflicts:
    #	app/src/main/java/com/chaitanya/sjsumap/MapActivity.java
    #	app/src/main/res/layout/activity_map.xml

--- app/src/main/java/com/chaitanya/sjsumap/MapActivity.java
+++ app/src/main/java/com/chaitanya/sjsumap/MapActivity.java
@@ -1,101 +1,43 @@
 package com.chaitanya.sjsumap;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.zip.Inflater;
 
 public class MapActivity extends AppCompatActivity {
-/*    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }*/
-
-    ArrayAdapter<String> adapter;
+    RelativeLayout relativeLayout;
+    ArrayList<Building> buildingList = new ArrayList<Building>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        AlertDialog.Builder builder =  new AlertDialog.Builder(MapActivity.this);
-        builder.setMessage("Map App Started " )
-                .setCancelable(false)
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
+        //Adding Building as per list given by professor
+        //Order is same as the list
+        buildingList.add(new Building(1,"MLK","MLK Address",new Coordinate(170,600),new Coordinate(310,850)));
+        buildingList.add(new Building(2,"ENG","ENG Address",new Coordinate(720,600),new Coordinate(920,900)));
+        buildingList.add(new Building(3,"YUH","YUH Address",new Coordinate(150,1210),new Coordinate(300,1420)));
+        buildingList.add(new Building(4,"SU","SU Address",new Coordinate(720,930),new Coordinate(1065,1070)));
+        buildingList.add(new Building(5,"BBC","BBC Address",new Coordinate(1110,1075),new Coordinate(1250,1215)));
+        buildingList.add(new Building(6,"SPG","SPG Address",new Coordinate(450,1675),new Coordinate(695,1910)));
+        relativeLayout = (RelativeLayout)findViewById(R.id.mainLayout);
+        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                System.out.println("(" + ((int)event.getX() - v.getLeft()) + "," + ((int)event.getY()- v.getTop()) + ")");
+                for(Building building: buildingList){
+                    if(building.checkTouch((int)event.getX() - v.getLeft(),(int)event.getY()- v.getTop())){
+                        Toast.makeText(MapActivity.this, building.name.toString(),Toast.LENGTH_SHORT).show();
                     }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-
-        ListView lv = (ListView) findViewById(R.id.listView);
-        ArrayList<String> arraySjsu = new ArrayList<>();
-
-        arraySjsu.addAll(Arrays.asList(getResources().getStringArray(R.array.array_sjsu)));
-
-        adapter = new ArrayAdapter<>(
-                MapActivity.this,
-                android.R.layout.simple_list_item_1,
-                arraySjsu);
-        lv.setAdapter(adapter);
-
-    }
-
-    @Override
-    public boolean onCreateOptionMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search,menu);
-        MenuItem item = menu.findItem(R.id.menuSearch);
-        SearchView searchView = (SearchView)item.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-
+                }
                 return false;
             }
         });
-
-        return super.onCreateOptionsMenu(menu);
-
     }
-
 }
